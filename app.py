6cfc58dd1e91a83350307900f6440ad59d107e4c
Ivan Sysoev <ivansysoev007@gmail.com>
2024-12-16 20:06:28 +0300

    Window size fix, audio repeat update

--- app.py
+++ app.py
@@ -19,23 +19,38 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('John Pork is calling...')
         self.setWindowIcon(QIcon(resource_path('assets/images/icon.png')))
 
-        # Воспроизведение звука
+        # Воспроизведение звука с повторением
         self.media_player = QMediaPlayer(self)
         url = QUrl.fromLocalFile(resource_path('assets/audios/audio.mp3'))
         content = QMediaContent(url)
         self.media_player.setMedia(content)
-        self.media_player.play()
+        self.media_player.mediaStatusChanged.connect(self.handleMediaStatusChanged)
 
         # Установка фона
         background_image = QImage(resource_path('assets/images/background_image.png'))
-        self.setGeometry(100, 100, background_image.width(), background_image.height())
+        
+        # Установка размера окна
+        screen_geometry = QApplication.desktop().screenGeometry()
+        new_width = int(background_image.width() // 1.5)
+        new_height = int(background_image.height() // 1.5)
+
+        self.setFixedSize(new_width, new_height)
+        x = (screen_geometry.width() - new_width) // 2
+        y = (screen_geometry.height() - new_height) // 2
+        
+        self.setGeometry(x, y, new_width, new_height)
         palette = QPalette()
-        palette.setBrush(QPalette.Window, QBrush(background_image))
+        palette.setBrush(QPalette.Window, QBrush(background_image.scaled(new_width, new_height)))
         self.setPalette(palette)
+
+    def handleMediaStatusChanged(self, status):
+        if status == QMediaPlayer.EndOfMedia:
+            self.media_player.setPosition(0)
+            self.media_player.play()
+        self.media_player.play()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
-
