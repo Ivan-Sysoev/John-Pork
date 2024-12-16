8a7e7a4ae67e6df28a45979e78f3c0a4c121c3e5
Ivan Sysoev <ivansysoev007@gmail.com>
2024-12-16 19:40:14 +0300

    Convert .py to .exe

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPalette, QBrush, QImage, QIcon
@@ -6,21 +7,27 @@ from PyQt5.QtGui import QPalette, QBrush, QImage, QIcon
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtCore import QUrl
 
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    else:
+        return os.path.join(os.path.abspath('.'), relative_path)
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('John Pork is calling...')
-        self.setWindowIcon(QIcon('assets/images/icon.png'))
+        self.setWindowIcon(QIcon(resource_path('assets/images/icon.png')))
 
         # Воспроизведение звука
         self.media_player = QMediaPlayer(self)
-        url = QUrl.fromLocalFile('assets/audios/audio.mp3')
+        url = QUrl.fromLocalFile(resource_path('assets/audios/audio.mp3'))
         content = QMediaContent(url)
         self.media_player.setMedia(content)
         self.media_player.play()
 
         # Установка фона
-        background_image = QImage('assets/images/background_image.png')
+        background_image = QImage(resource_path('assets/images/background_image.png'))
         self.setGeometry(100, 100, background_image.width(), background_image.height())
         palette = QPalette()
         palette.setBrush(QPalette.Window, QBrush(background_image))
@@ -31,3 +38,4 @@ if __name__ == '__main__':
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
